03a6cd33fa00d74475a793c853e08b752c8cd990
Vabolos <vaboloslol@gmail.com>
2025-02-13 16:07:47 +0100

    Update document title from "Navigation" to "Github"

--- dist/scripts/settings/documents.mjs
+++ dist/scripts/settings/documents.mjs
@@ -22,7 +22,7 @@ export const Documents = [
         spacer: true,
     },
     {
-        title: "Navigation",
+        title: "Github",
         href: "/navigation",
         heading: "Documents",
     },
